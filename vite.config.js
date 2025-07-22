e4160760085292bbb6293b9573099c16ffc75886
Jam <jamahl@roomxvi.com>
2025-07-22 15:12:12 +0100

    Build for root path on GH Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   // Base path for GitHub Pages deployment
-  base: '/jbfinejewellery/',
+  base: '/',
   plugins: [react()],
   server: {
     port: 5173,
